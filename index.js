b4c3d09de5ea47ed2ae05d5926245b1da0e3101c
grunjol <grunjol@argenteam.net>
2016-04-15 18:57:30 -0300

    add newline and detectSize to refine input text detection limits

--- index.js
+++ index.js
@@ -18,18 +18,20 @@ function getSupportedEncoding (encoding) {
 
 function toutf8 (opts) {
   if(!opts) opts = {}
-  if(typeof encoding == 'string') opts = { encoding: opts }
+  if(typeof opts === 'string') opts = { encoding: opts }
   var conf = opts.confidence || 0
+  var newline = opts.newline !== false
+  var detectSize = opts.detectSize || 65535
   var encoding = opts.encoding
   // encoding given
   if(encoding) return convertFrom(encoding)
-    
+
   // detect encoding first
-  return peek(function (data, swap) {
+  return peek({newline: newline, maxBuffer: detectSize}, function (data, swap) {
     if(!Buffer.isBuffer(data)) return swap(new Error('No buffer'))
     var matches = detect(data)
     var encoding = matches.length > 0 && matches[0].confidence > conf
-      ? matches[0].charsetName 
+      ? matches[0].charsetName
       : 'utf8'
     encoding = getSupportedEncoding(encoding)
     swap(null, convertFrom(encoding))
